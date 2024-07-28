a1db813a38fdf3493c39d8fa6f8cf8df9168d289
Gedeon Sainrival <hello@gedeondoescode.com>
2024-07-28 13:41:51 -0400

    fix foreach loop

--- Module 7/project.cs
+++ Module 7/project.cs
@@ -4,129 +4,130 @@ using System.Collections.Generic;
 
 class Program
 {
-   static public void Main () 
-   {
-    Dictionary<int, string> Store = new Dictionary<int, string>();
-      char choice;
-      do{
-      Console.WriteLine("\n Grocery Store Options");
-      Console.WriteLine("a. Populate the Grocery List");
-      Console.WriteLine("b. Display the Grocery Contents");
-      Console.WriteLine("c. Remove an Item"); // remove a key
-      Console.WriteLine("d. Add a new item and value"); // add a new key and value
-      Console.WriteLine("e. Add value to existing item"); // add a value to an existing key
-      Console.WriteLine("f. Sort out grocery items"); //sort the keys in the dictionary
-      Console.WriteLine("g. Exit Grocery Store"); //end program
+    static public void Main()
+    {
+        Dictionary<int, string> Store = new Dictionary<int, string>();
+        char choice;
+        do
+        {
+            Console.WriteLine("\n Grocery Store Options");
+            Console.WriteLine("a. Populate the Grocery List");
+            Console.WriteLine("b. Display the Grocery Contents");
+            Console.WriteLine("c. Remove an Item"); // remove a key
+            Console.WriteLine("d. Add a new item and value"); // add a new key and value
+            Console.WriteLine("e. Add value to existing item"); // add a value to an existing key
+            Console.WriteLine("f. Sort out grocery items"); //sort the keys in the dictionary
+            Console.WriteLine("g. Exit Grocery Store"); //end program
 
-      Console.WriteLine("\n Please enter your choice: "); //prompts the user to enter and perform any of these tasks
-      choice = Char.ToLower(Console.ReadLine()[0]); 
-         switch(choice)
-      {
-         case 'a':
-            PopulateGroceryDict(Store);
-            break;
-         case 'b':
-            DisplayDictContents(Store);
-            break;
-         case 'c':
-            RemoveContents(Store);
-            break;
-         case 'd':
-            AddItems(Store);
-            break;
-         case 'e':
-            AddValueExisting(Store);
-            break;
-         case 'f':
-            SortDictionary(Store);
-            break;
-         case 'g':
-            break;
-         default:
-            Console.WriteLine("Invalid choice. Please try again.");
-            break;
-      }
-   } while(choice != 'g');
-   Console.WriteLine("Thank you for using this Grocery Store. Come Shop Again!");
-}
+            Console.WriteLine("\n Please enter your choice: "); //prompts the user to enter and perform any of these tasks
+            choice = Char.ToLower(Console.ReadLine()[0]);
+            switch (choice)
+            {
+                case 'a':
+                    PopulateGroceryDict(Store);
+                    break;
+                case 'b':
+                    DisplayDictContents(Store);
+                    break;
+                case 'c':
+                    RemoveContents(Store);
+                    break;
+                case 'd':
+                    AddItems(Store);
+                    break;
+                case 'e':
+                    AddValueExisting(Store);
+                    break;
+                case 'f':
+                    SortDictionary(Store);
+                    break;
+                case 'g':
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        } while (choice != 'g');
+        Console.WriteLine("Thank you for using this Grocery Store. Come Shop Again!");
+    }
 
-static void PopulateGroceryDict(Dictionary<int, string> Store)
-   {
-      Store.Clear();
-      Store.Add(2, "Milk Cartons");
-      Store.Add(12, "Eggs");
-      Store.Add(1, "Bread");
-      Store.Add(3, "Bananas");
-      Store.Add(6, "Apples");
-      Store.Add(4, "Oranges");
-      Store.Add(5, "Tomatoes");
-      Store.Add(9, "Onions");
-      Store.Add(28, "Potatoes");
-      Store.Add(19, "Carrots");
-      Console.WriteLine("Grocery List has been populated with items!");
-   }
-static void DisplayDictContents(Dictionary<int, string> Store)
-   {
-      Console.WriteLine("Grocery Store Contents:");
-      
-      foreach(var(key, value) in Store)
-      {
-         Console.WriteLine($"{key}, {value}");
-      }
-      
-   }
-static void RemoveContents(Dictionary<int, string> Store)
-   {
-      Console.WriteLine("Enter the quantity of the item you want to remove: ");
-      int quantity = int.Parse(Console.ReadLine());
-      if (Store.ContainsKey(quantity))
-      {
-         Store.Remove(quantity);
-         Console.WriteLine("Item removed successfully.");
-      }
-      else
-      {
-         Console.WriteLine("Item not found in the dictionary.");
-      }
-   }
-static void AddItems(Dictionary<int, string> Store)   
-   {
-      Console.WriteLine("Enter the quantity of the item you want to add: ");
-      int quantity = int.Parse(Console.ReadLine());
-      
-      Console.WriteLine("Enter the name of item you want to add: ");
-      string item = Console.ReadLine();
- 
-      Store.Add(quantity, item);
-      Console.WriteLine("Item added successfully!");
-      
-   }
-static void AddValueExisting(Dictionary<int, string> Store)
-   {
-      Console.WriteLine("Enter the quantity of the item you want to add to:");
-      int quantity = int.Parse(Console.ReadLine());
-      
-      if (Store.ContainsKey(quantity))
-      {
-         Console.WriteLine("Enter the value you want to add to the item:");
-         string value = Console.ReadLine();
+    static void PopulateGroceryDict(Dictionary<int, string> Store)
+    {
+        Store.Clear();
+        Store.Add(2, "Milk Cartons");
+        Store.Add(12, "Eggs");
+        Store.Add(1, "Bread");
+        Store.Add(3, "Bananas");
+        Store.Add(6, "Apples");
+        Store.Add(4, "Oranges");
+        Store.Add(5, "Tomatoes");
+        Store.Add(9, "Onions");
+        Store.Add(28, "Potatoes");
+        Store.Add(19, "Carrots");
+        Console.WriteLine("Grocery List has been populated with items!");
+    }
+    static void DisplayDictContents(Dictionary<int, string> Store)
+    {
+        Console.WriteLine("Grocery Store Contents:");
 
-         Store[quantity] += "- appended value: " + value;
-         Console.WriteLine("Your new value has been added successfully.");
-      }
-      else
-      {
-         Console.WriteLine("Please enter a valid key. Item not found in dictionary.");
-      }   
-   }
-   static void SortDictionary(Dictionary<int, string> Store)
-   {
-      var sortedStoreByValue = Store.OrderBy(item => item.Key).ToList();
-      Console.WriteLine("Grocery Store Contents (Sorted):");
-      foreach(var item in sortedStoreByValue)
-      {
-         Console.WriteLine($"{item.Key}-{item.Value}");
-      }
-   }
+        foreach (var item in Store)
+        {
+            Console.WriteLine($"{item.Key}, {item.Value}");
+        }
+
+    }
+    static void RemoveContents(Dictionary<int, string> Store)
+    {
+        Console.WriteLine("Enter the quantity of the item you want to remove: ");
+        int quantity = int.Parse(Console.ReadLine());
+        if (Store.ContainsKey(quantity))
+        {
+            Store.Remove(quantity);
+            Console.WriteLine("Item removed successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Item not found in the dictionary.");
+        }
+    }
+    static void AddItems(Dictionary<int, string> Store)
+    {
+        Console.WriteLine("Enter the quantity of the item you want to add: ");
+        int quantity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the name of item you want to add: ");
+        string item = Console.ReadLine();
+
+        Store.Add(quantity, item);
+        Console.WriteLine("Item added successfully!");
+
+    }
+    static void AddValueExisting(Dictionary<int, string> Store)
+    {
+        Console.WriteLine("Enter the quantity of the item you want to add to:");
+        int quantity = int.Parse(Console.ReadLine());
+
+        if (Store.ContainsKey(quantity))
+        {
+            Console.WriteLine("Enter the value you want to add to the item:");
+            string value = Console.ReadLine();
+
+            Store[quantity] += "- appended value: " + value;
+            Console.WriteLine("Your new value has been added successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid key. Item not found in dictionary.");
+        }
+    }
+    static void SortDictionary(Dictionary<int, string> Store)
+    {
+        var sortedStoreByValue = Store.OrderBy(item => item.Key).ToList();
+        Console.WriteLine("Grocery Store Contents (Sorted):");
+        foreach (var item in sortedStoreByValue)
+        {
+            Console.WriteLine($"{item.Key}-{item.Value}");
+        }
+    }
 
 }
